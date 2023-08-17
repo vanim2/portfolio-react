d81ac7884f19c12d283858f4a846872a2cba3a2d
vanim2 <125849083+vanim2@users.noreply.github.com>
2023-08-17 19:51:27 -0400

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,15 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  </BrowserRouter>
 );
-
 
 reportWebVitals();
